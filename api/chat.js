67a14d3a4898124ecd228d5fa6d57a06ec80b021
Patricia Gonzalez <pat.gonzalezz04@gmail.com>
2026-05-21 22:57:19 -0600

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -3,7 +3,8 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
+  "x-api-key": process.env.ANTHROPIC_API_KEY,
+if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
 
