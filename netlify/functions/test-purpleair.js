c2d62cae26c8fd3d9d83c692a72bbe67cbf5414a
Patricia Gonzalez <pat.gonzalezz04@gmail.com>
2026-09-10 10:04:08 -0600

    Add PurpleAir API integration in test-purpleair.js

--- netlify/functions/test-purpleair.js
+++ netlify/functions/test-purpleair.js
@@ -15,3 +15,26 @@ exports.handler = async function (event, context) {
     body: JSON.stringify(data, null, 2)
   };
 };
+exports.handler = async function (event, context) {
+  const key = process.env.PURPLEAIR_API_KEY;
+
+  const response = await fetch(
+    "https://api.purpleair.com/v1/sensors?fields=name,latitude,longitude,pm2.5_atm,last_seen&nwlat=31.755&nwlng=-106.475&selat=31.735&selng=-106.440",
+    {
+      headers: {
+        "X-API-Key": key
+      }
+    }
+  );
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      key_exists: !!key,
+      key_length: key ? key.length : 0,
+      key_first_4_chars: key ? key.slice(0, 4) : null,
+      result: data
+    }, null, 2)
+  };
