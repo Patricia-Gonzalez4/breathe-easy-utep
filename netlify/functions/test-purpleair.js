29129dc922f1cfafef0f2dec377f303911dbe3f3
Patricia Gonzalez <pat.gonzalezz04@gmail.com>
2026-09-10 10:51:34 -0600

    Refactor PurpleAir API handler
    Refactor handler to use API key variable and return structured response.

--- netlify/functions/test-purpleair.js
+++ netlify/functions/test-purpleair.js
@@ -1,21 +1,4 @@
 exports.handler = async function (event, context) {
-  const response = await fetch(
-    "https://api.purpleair.com/v1/sensors?fields=name,latitude,longitude,pm2.5_atm,last_seen&nwlat=31.755&nwlng=-106.475&selat=31.735&selng=-106.440",
-    {
-      headers: {
-        "X-API-Key": process.env.PURPLEAIR_API_KEY
-      }
-    }
-  );
-
-  const data = await response.json();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data, null, 2)
-  };
-};
-exports.handler = async function (event, context) {
   const key = process.env.PURPLEAIR_API_KEY;
 
   const response = await fetch(
@@ -38,3 +21,4 @@ exports.handler = async function (event, context) {
       result: data
     }, null, 2)
   };
+};
