41815b95b09a7b9b63851aaee09cf9965962c914
Patricia Gonzalez <pat.gonzalezz04@gmail.com>
2026-09-10 11:31:37 -0600

    Fix formatting and add missing newline in test-purpleair.js

--- netlify/functions/test-purpleair.js
+++ netlify/functions/test-purpleair.js
@@ -2,7 +2,8 @@ exports.handler = async function (event, context) {
   const key = process.env.PURPLEAIR_API_KEY;
 
   const response = await fetch(
-    ""https://api.purpleair.com/v1/sensors?fields=name,latitude,longitude,pm2.5_atm,last_seen&nwlat=31.85&nwlng=-106.60&selat=31.65&selng=-106.30",
+    "https://api.purpleair.com/v1/sensors?fields=name,latitude,longitude,pm2.5_atm,last_seen&nwlat=31.85&nwlng=-106.60&selat=31.65&selng=-106.30",
+    {
       headers: {
         "X-API-Key": key
       }
